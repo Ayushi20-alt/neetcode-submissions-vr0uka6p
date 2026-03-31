9e3a125a537cf8e2f0f535fb2b643208cce23955
Ayushi SIngh <107174083+Ayushi20-alt@users.noreply.github.com>
2026-03-31 23:58:26 +0530

    Add: evaluate-division - submission-0

---
+++ Data Structures & Algorithms/evaluate-division/submission-0.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    void dfs(unordered_map<string, vector<pair<string, double>>>&adj, string src, string dst,
+    unordered_set<string>&visited, double product, double &ans){
+        if(visited.find(src) != visited.end()){
+            return;
+        }
+        visited.insert(src);
+        if(src == dst){
+            ans = product;
+            return;
+        }
+
+        for(auto &it : adj[src]){
+            string v = it.first;
+            double val = it.second;
+
+            dfs(adj, v, dst, visited, product * val, ans);
+        }
+    }
+    vector<double> calcEquation(vector<vector<string>>& equations, 
+        vector<double>& values, vector<vector<string>>& queries) {
+        int n = equations.size();
+        // make graph
+        unordered_map<string, vector<pair<string, double>>>adj;
+        for(int i = 0; i < n; i++){
+            string u = equations[i][0]; // a
+            string v = equations[i][1]; // b
+            double val = values[i]; // a/b
+            adj[u].push_back({v, val});
+            adj[v].push_back({u, 1.0/val});
+        }
+
+        vector<double> result;
+        for(auto &it : queries){
+            string src = it[0];
+            string dst = it[1];
+
+            double ans = -1.0;
+            double product = 1.0;
+
+            if(adj.find(src) != adj.end()){
+                unordered_set<string>visited;
+                dfs(adj, src, dst, visited, product, ans);
+            }
+
+            result.push_back(ans);
+        }
+        return result;
+    }
+};
