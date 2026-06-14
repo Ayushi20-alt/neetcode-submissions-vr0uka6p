334d1a3b08a5560659683820bdee4f6cc872dd55
Ayushi SIngh <107174083+Ayushi20-alt@users.noreply.github.com>
2026-06-14 18:46:40 +0530

    Add: swim-in-rising-water - submission-0

---
+++ Data Structures & Algorithms/swim-in-rising-water/submission-0.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    bool possibleToReach(int i , int j, int t, vector<vector<int>>& grid, vector<vector<bool>>&vis){
+        int n = grid.size();
+        if(i < 0 || i >= n || j < 0 || j >= n || vis[i][j] == true || grid[i][j] > t){
+            return false;
+        }
+
+        vis[i][j] = true;
+        if(i == n-1 && j == n-1) return true;
+
+        int delrow[] = {-1, 0, 1, 0};
+        int delcol[] = {0, 1, 0, -1};
+
+        for(int k = 0; k < 4; k++){
+            int nrow = i + delrow[k];
+            int ncol = j + delcol[k];
+
+            if(possibleToReach(nrow, ncol, t, grid, vis)) return true;
+        }
+
+        return false;
+    }
+    int swimInWater(vector<vector<int>>& grid) {
+        int n = grid.size();
+        int l = grid[0][0];
+        int r = n * n -1;
+        int result = 0;
+
+        while(l <= r){
+            // kya mid time tk hum pahunch payege ?
+            int mid = l + (r-l)/2;
+
+            vector<vector<bool>>vis(n, vector<bool>(n, false));
+            if(possibleToReach(0, 0, mid, grid, vis)){
+                result = mid;
+                r = mid - 1;
+            }else{
+                l = mid + 1;
+            }
+        }
+
+        return result;
+    }
+};
