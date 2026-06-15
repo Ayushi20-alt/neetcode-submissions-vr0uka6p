c679f5d9533461b2f7812392d8b77ad99b0c422a
Ayushi SIngh <107174083+Ayushi20-alt@users.noreply.github.com>
2026-06-16 00:50:11 +0530

    Add: palindromic-substrings - submission-0

---
+++ Data Structures & Algorithms/palindromic-substrings/submission-0.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool solve(string &s, int i, int j){
+        if(i >= j) return true;
+
+        if(s[i] == s[j]) {
+            return solve(s, i+1, j-1);
+        }else{
+            return false;
+        }
+    }
+    int countSubstrings(string s) {
+        int n = s.size();
+        int count = 0;
+
+        for(int i = 0; i < n; i++){
+            for(int j = i; j < n; j++){
+                if(solve(s, i, j) == true){
+                    count ++;
+                }
+            }
+        }
+        
+        return count;
+    }
+};
