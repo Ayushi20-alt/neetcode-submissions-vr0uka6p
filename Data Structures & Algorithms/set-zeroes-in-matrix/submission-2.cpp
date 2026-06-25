df5160960112790a32632fb8d6d1de1b1ea4670f
Ayushi SIngh <107174083+Ayushi20-alt@users.noreply.github.com>
2026-06-25 23:35:30 +0530

    Add: set-zeroes-in-matrix - submission-2

---
+++ Data Structures & Algorithms/set-zeroes-in-matrix/submission-2.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+        int col0 = 1;
+
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(matrix[i][j] == 0){
+                    matrix[i][0] = 0;
+                    if(j != 0) matrix[0][j] = 0;
+                    else col0 = 0;
+                }
+            }
+        }
+
+        // now traversing to mark them 0
+        // firstly mark the inner matrix as 0 and then 1st col and then 1st row
+
+        for(int i = 1; i < m; i++){
+            for(int j = 1; j < n; j++){
+                if(matrix[i][j] != 0){
+                    if(matrix[0][j] == 0 || matrix[i][0] == 0){
+                        matrix[i][j] = 0;
+                    }
+                }
+            }
+        }
+
+        if(matrix[0][0] == 0){
+            for(int i = 0; i < n; i++) matrix[0][i] = 0;
+        }
+
+        if(col0 == 0){
+            for(int i = 0; i < m; i++) matrix[i][0] = 0;
+        }
+    }
+};
